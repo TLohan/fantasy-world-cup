d36ef48931251da28ab7407ed9c7edee09c66fc1
Tom Lohan <tlohan89@gmail.com>
2018-06-19 11:06:36 +0100

    working with fifa api

---
+++ src/app/home/live-fixture.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FifaFixture } from 'models/fifa-fixture.model';
+import { LivescoreService } from '../services/livescore.service';
+import { Player } from 'models/player.model';
+import { Goal } from 'models/goal.model';
+
+@Component({
+    selector: 'app-live-fixture',
+    templateUrl: 'live-fixture.component.html',
+    styleUrls: ['fifa-data.component.scss']
+})
+export class LiveFixtureComponent implements OnInit {
+    private _fixture: FifaFixture;
+
+    @Input() fixture: FifaFixture = new FifaFixture(null);
+
+    constructor(private livescoreService: LivescoreService) {
+    }
+    
+    ngOnInit() {
+        console.log(this.fixture.AwayTeamName);
+        console.log(this.fixture.MatchStatus);
+        
+        this.livescoreService.getLiveFixtureData(this.fixture).subscribe(data => {
+            const homeTeamPlayersData = data.HomeTeam.Players;
+            const awayTeamPlayers = data.AwayTeam.Players;
+            const homeGoalsData = data.HomeTeam.Goals;
+            const awayGoalsData = data.AwayTeam.Goals;
+            const players: Player[] = [];
+            const homeGoals: Goal[] = [];
+            const awayGoals: Goal[] = [];
+
+            this.fixture.MatchTime = data.MatchTime;
+
+            homeTeamPlayersData.forEach(player => {
+                players.push(new Player(player));
+            });
+
+            awayTeamPlayers.forEach(player => {
+                players.push(new Player(player));
+            });
+
+            homeGoalsData.forEach(goal => {
+                const gol = new Goal(goal);
+                gol.PlayerName = players.find(function (player) { return player.IdPlayer === goal.IdPlayer; }).Name;
+                homeGoals.push(gol);
+            });
+
+            awayGoalsData.forEach(goal => {
+                const gol = new Goal(goal);
+                gol.PlayerName = players.find(function (player) { return player.IdPlayer === goal.IdPlayer; }).Name;
+                awayGoals.push(gol);
+            });
+            this.fixture.HomeGoals = homeGoals;
+            this.fixture.AwayGoals = awayGoals;
+        });
+    }
+
+}
---
+++ src/app/services/livescore.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { FifaFixture } from 'models/fifa-fixture.model';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class LivescoreService {
+
+    constructor(private http: Http) {}
+
+    getLiveFixtureData(fixture: FifaFixture): Observable<any> {
+        const url = `https://api.fifa.com/api/v1/live/football/17/254645/${fixture.IdStage}/${fixture.IdMatch}?language=en-GB`;
+        return this.http.get(url).map((response: Response) => {
+            response = response.json();
+            return response;
+        });
+    }
+}
